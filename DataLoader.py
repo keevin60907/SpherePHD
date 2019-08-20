2e90bdb4e240fe617f4a5f60b2c32e676586e0e3
keevin60907 <b03203048@ntu.edu.tw>
2019-08-20 20:39:49 +0800

    run on Stanford2D3D

--- DataLoader.py
+++ DataLoader.py
@@ -1,8 +1,13 @@
 import os
 import numpy as np
+import cv2
+from utils import *
 from makedata import *
 from maketable import *
 
+CLASS_LIST = ['<UNK>', 'beam', 'board', 'bookcase', 'ceiling', 'chair', 'clutter',
+              'column', 'door', 'floor', 'sofa', 'table', 'wall', 'window']
+ID_LIST = [1, 160, 297, 881, 1266, 2629, 6511, 6765, 7308, 7592, 7647, 8102, 9648, 9816]
 np.random.seed(1513)
 
 class DataLoader():
@@ -10,25 +15,92 @@ class DataLoader():
     def __init__ (self, dir_path, subdivision):
         # Process the panoramic picture
         self.dataset = []
-        cnt = 0;
+        cnt = 1
         for file in sorted(os.listdir(dir_path)):
             if file[-4:] == '.png' or file[-4:] == '.jpg':
                 # subdivision defualt = 8
-                self.dataset.append(pano2icosa(os.path.join(dir_path, file), subdivision))
+                img = cv2.imread(os.path.join(dir_path, file))
+                img = cv2.resize(img, (400, 200))
+                self.dataset.append(pano2icosa(img, subdivision))
                 print('Process %05d pictures..\r' %cnt, end='')
                 cnt += 1
 
         self.dataset = np.array(self.dataset) # shape = (N, 4**8 * 20, 3)
+        self.dataset = self.dataset / 255
         self.num_data = self.dataset.shape[0]
 
     def shuffle(self):
         self.rng = np.random.shuffle(np.arange(self.num_data))
         self.dataset = self.dataset[self.rng]
 
+    def make_label(self, subdivision):
+        reference = load_labels('./assets/semantic_labels.json')
+        cnt = 1
+        ret = []
+        for file in sorted(os.listdir('/media/bl530/新增磁碟區/area_1/pano/semantic/')):
+            if file[-4:] == '.png' or file[-4:] == '.jpg':
+                img = cv2.imread(os.path.join('/media/bl530/新增磁碟區/area_1/pano/semantic/', file))
+                img = cv2.resize(img, (400, 200))
+                img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+                tmp = np.zeros((img.shape[0], img.shape[1], 1))-1
+                print('Label %05d pictures..\r' %cnt, end='')
+                index = img[:, :, 0]*256*256 + img[:, :, 1]*256 + img[:, :, 2]
+                index = np.expand_dims(index, axis=2)
+                index[index > 9816] = 0
+                for i in range(len(CLASS_LIST)):
+                    tmp[(index < ID_LIST[i]) & (tmp == -1)] = i
+                ret.append(pano2icosa(tmp, subdivision))
+                cnt += 1
+        print('')
+        return np.array(ret)
 
 def main():
-    pano_data = DataLoader('/media/bl530/新增磁碟區/area_1/pano/rgb/', 8)
-    np.save('./area_1.npy', pano_data.dataset)
+    pano_data_1 = DataLoader('/media/bl530/新增磁碟區/area_1/pano/rgb/', 6)
+    print('')
+    pano_data_2 = DataLoader('/media/bl530/新增磁碟區/area_2/pano/rgb/', 6)
+    print('')
+    pano_data_3 = DataLoader('/media/bl530/新增磁碟區/area_3/pano/rgb/', 6)
+    print('')
+    pano_data_4 = DataLoader('/media/bl530/新增磁碟區/area_4/pano/rgb/', 6)
+    print('')
+    pano_data_6 = DataLoader('/media/bl530/新增磁碟區/area_6/pano/rgb/', 6)
+    print('')
+    train_data = np.concatenate([pano_data_1.dataset, pano_data_2.dataset, pano_data_3.dataset,\
+                                 pano_data_4.dataset, pano_data_6.dataset], axis=0)
+    np.save('./train_data.npy', train_data)
+    print('')
+
+    label_1 = pano_data_1.make_label(6)
+    print('')
+    label_2 = pano_data_2.make_label(6)
+    print('')
+    label_3 = pano_data_3.make_label(6)
+    print('')
+    label_4 = pano_data_4.make_label(6)
+    print('')
+    label_6 = pano_data_6.make_label(6)
+    print('')
+    train_label = np.concatenate([label_1, label_2, label_3, label_4, label_6], axis=0)
+    print('')
+    np.save('./train_label.npy', train_label)
+    print('')
+
+    pano_data_5a = DataLoader('/media/bl530/新增磁碟區/area_5a/pano/rgb/', 6)
+    print('')
+    pano_data_5b = DataLoader('/media/bl530/新增磁碟區/area_5b/pano/rgb/', 6)
+    print('')
+    train_data = np.concatenate([pano_data_5a.dataset, pano_data_5b.dataset], axis=0)
+    np.save('./test_data.npy', test_data)
+    print('')
+
+    label_5a = pano_data_5a.make_label(6)
+    print('')
+    label_5b = pano_data_5b.make_label(6)
+    print('')
+    train_label = np.concatenate([label_5a, label_5b], axis=0)
+    print('')
+    np.save('./test_label.npy', test_label)
+    print('')
 
 if __name__ == '__main__':
     main()
--- train.py
+++ train.py
@@ -6,40 +6,50 @@ import cv2
 
 import DataLoader
 from maketable import *
-from layer import *
+from makedata import * 
+from model import *
 from loadData import *
 
 np.set_printoptions(threshold=0.1)
 os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 
-SUBDIVISION = 3
-NUM_CLASS = 10
+SUBDIVISION = 7
+NUM_CLASS = 14
 
 class SpherePHD():
 
     def autoencoder(self, subdivision):
         self.conv_tables = []
         self.adj_tables = []
-        for i in range(0, subdivision+1):
-            self.conv_tables.append(make_conv_table(i))
-            self.adj_tables.append(make_adjacency_table(i))
+        self.upsample_tables = []
         self.pooling_tables = make_pooling_table(subdivision+1)
+        for div in range(0, subdivision+1):
+            conv_table = make_conv_table(div)
+            self.conv_tables.append(conv_table)
+            adj_table = make_adjacency_table(div)
+            self.adj_tables.append(adj_table)
+            if div > 0:
+                upsample_table = make_upsample_table(self.pooling_tables[div-1], adj_table)
+                self.upsample_tables.append(upsample_table)
+        self.image_size = 20 * 4 ** subdivision 
 
-        self.num_epochs = 150
-        self.num_steps = 3000
-        self.batch_size = 20
+        self.num_epochs = 500
+        self.num_steps = 150
+        self.batch_size = 1
         self.X = tf.placeholder(tf.float32, [None, 1, 20 * 4**subdivision, 3])
-        self.Y = tf.placeholder(tf.float32, [None, 1, 20 * 4**subdivision, NUM_CLASS])
+        self.Y = tf.placeholder(tf.float32, [None, 20 * 4**subdivision, NUM_CLASS])
 
-        self.logits = MNIST_net(self.X, self.conv_tables, self.adj_tables, self.pooling_tables, subdivision)
+        self.logits = auto_encoder(self.X, self.conv_tables, self.adj_tables,\
+                                   self.pooling_tables, self.upsample_tables, subdivision)
         self.prediction = tf.nn.softmax(self.logits)
+        self.prediction = tf.squeeze(self.prediction, axis=1)
 
         self.loss_op = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=self.logits, labels=self.Y))
         self.optimizer = tf.train.AdamOptimizer(learning_rate=0.0001)
         self.train_op = self.optimizer.minimize(self.loss_op)
 
         # Evaluate model
-        correct_pred = tf.equal(tf.argmax(self.prediction, 1), tf.argmax(self.Y, 1))
+        correct_pred = tf.equal(tf.argmax(self.prediction, 2), tf.argmax(self.Y, 2))
         self.accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
 
         self.saver = tf.train.Saver()
@@ -56,13 +66,13 @@ class SpherePHD():
             self.conv_tables.append(make_conv_table(i))
             self.adj_tables.append(make_adjacency_table(i))
         self.pooling_tables = make_pooling_table(subdivision+1)
-
+        self.image_size = 20 * 4 ** subdivision 
 
         self.num_epochs = 150
         self.num_steps = 3000
         self.batch_size = 20
         self.X = tf.placeholder(tf.float32, [None, 1, 20 * 4**subdivision, 1])
-        self.Y = tf.placeholder(tf.float32, [None, NUM_CLASS])
+        self.Y = tf.placeholder(tf.float32, [None, 20 * 4**subdivision, NUM_CLASS])
 
         self.logits = MNIST_net(self.X, self.conv_tables, self.adj_tables, self.pooling_tables, subdivision)
         self.prediction = tf.nn.softmax(self.logits)
@@ -82,82 +92,117 @@ class SpherePHD():
         self.config.gpu_options.per_process_gpu_memory_fraction = 1
         self.config.gpu_options.allow_growth = True
 
-    def train(self):
+    def train(self, restore=False):
+
+        train_images = np.load('./area_1.npy')[:150]
+        train_labels = np.load('./area_1_label.npy')[:150]
+        train_one_hot = np.zeros((train_labels.shape[0], train_labels.shape[1], 14))
+        for i in range(train_labels.shape[0]):
+            for j in range(train_labels.shape[1]):
+                train_one_hot[i, j, int(train_labels[i, j])] = 1
 
         # Training loop
         batch_size = self.batch_size
-        save_file_path = './save/MNIST_Sphere_model_'
+        save_file_path = './save/Stanford2D3D_Sphere_model_'
         sess = tf.Session(config=self.config)
         sess = tf.Session()
         init = tf.global_variables_initializer()
         sess.run(init)
+        if restore:
+            self.saver.restore(sess, './save/Stanford2D3D_Sphere_model_224.ckpt')
         print('Training start')
         for epoch in range(self.num_epochs):
             print('epoch {}'.format(epoch))
-            for idx in range(1,2):
-                train_images, train_labels = loadTrainData(idx)
-                for step in range(1, self.num_steps + 1):
-                    batch_x, batch_y = np.reshape(train_images[(step - 1) * batch_size:step * batch_size].astype(np.float32),
-                                                  [batch_size, 1, 1280, 1]), train_labels[
-                                                  (step - 1) * batch_size:step * batch_size].astype(np.float32)
+            for step in range(1, self.num_steps + 1):
+                batch_x, batch_y = np.reshape(train_images[(step - 1) * batch_size:step * batch_size].astype(np.float32),
+                                              [batch_size, 1, self.image_size, 3]),\
+                                              train_one_hot[(step - 1) * batch_size:step * batch_size].astype(np.float32)
 
-                    sess.run(self.train_op, feed_dict={self.X: batch_x, self.Y: batch_y})
+                sess.run(self.train_op, feed_dict={self.X: batch_x, self.Y: batch_y})
 
-                    if step % 400 == 0:
-                        loss, acc = sess.run([self.loss_op, self.accuracy], feed_dict={self.X: batch_x, self.Y: batch_y})
-                        print('Step ' + str((idx-1)*self.num_steps+step) + 
-                              ', Minibatch Loss= ' +'{:.4f}'.format(loss) + 
-                              ', Training Accuracy= ' + '{:.3f}'.format(acc)+'\r', end='')
+                if step % 10 == 0:
+                    loss, acc = sess.run([self.loss_op, self.accuracy], feed_dict={self.X: batch_x, self.Y: batch_y})
+                    print('Step ' + str(step) + 
+                          ', Minibatch Loss= ' +'{:.4f}'.format(loss) + 
+                          ', Training Accuracy= ' + '{:.3f}'.format(acc)+'\r', end='')
 
             print()
             save_path = self.saver.save(sess, save_file_path+str(epoch)+'.ckpt')
-            self.print_test(sess)
+            if epoch % 10 == 0:
+                self.print_test(sess, epoch, save_img=True)
+            else:
+                self.print_test(sess, epoch)
+
         print('Model saved in path: %s' % save_path)
         print('Training finish')
 
-    def print_test(self, sess):
+    def print_test(self, sess, epoch, save_img=False):
 
+        result = []
         batch_size = self.batch_size
         total_accuracy = 0
-        for idx in range(1,2):
-            test_images, test_labels = loadTestData(idx)
-            for step in range(1, 501):
-                batch_x, batch_y = np.reshape(test_images[(step - 1) * batch_size:step * batch_size].astype(np.float32),
-                                              [batch_size, 1, 1280, 1]), test_labels[
-                                              (step - 1) * batch_size:step * batch_size].astype(np.float32)
-                acc = sess.run([self.accuracy], feed_dict={self.X: batch_x, self.Y: batch_y})
-                total_accuracy = total_accuracy + acc[0]
+        test_images = np.load('./area_1.npy')[150:]
+        test_labels = np.load('./area_1_label.npy')[150:]
+        test_one_hot = np.zeros((test_labels.shape[0], test_labels.shape[1], 14))
+        for i in range(test_labels.shape[0]):
+            for j in range(test_labels.shape[1]):
+                test_one_hot[i, j, int(test_labels[i, j])] = 1
+        
+        for step in range(1, 41):
+            batch_x, batch_y = np.reshape(test_images[(step - 1) * batch_size:step * batch_size].astype(np.float32),
+                                          [batch_size, 1, self.image_size, 3]),\
+                                          test_one_hot[(step - 1) * batch_size:step * batch_size].astype(np.float32)
+            acc = sess.run([self.accuracy, self.prediction], feed_dict={self.X: batch_x, self.Y: batch_y})
+            total_accuracy = total_accuracy + acc[0]
+            result.append(np.array(acc[1]))
         total_accuracy *= batch_size
-        print('Testing accuracy is {:.2f}%'.format(total_accuracy/100))
+        print('Testing accuracy is {:.4f}'.format(total_accuracy/40))
+
+        result = np.array(result)
+        np.save('prediction.npy', result)
+
+        if save_img:
+            img = result[0].reshape(-1, 14)
+            icosa2pano(img, 'sub_6.log', './reconstruct_image/segment_'+str(epoch)+'.jpg', paint_seg=True)
 
     def test(self):
 
+        result = []
         batch_size = self.batch_size
         sess = tf.Session(config=self.config)
         sess = tf.Session()
         init = tf.global_variables_initializer()
         sess.run(init)
-        self.saver.restore(sess, './save/MNIST_Sphere_model_149.ckpt')
+        self.saver.restore(sess, './save/Stanford2D3D_Sphere_model_149.ckpt')
         #print("Testing start")
         total_accuracy = 0
         for idx in range(1,2):
-            test_images, test_labels = loadTestData(idx)
-            for step in range(1, 501):
+            test_images = np.load('./area_1.npy')[150:]
+            test_labels = np.load('./area_1_label.npy')[150:]
+            for step in range(1, 41):
                 batch_x, batch_y = np.reshape(test_images[(step - 1) * batch_size:step * batch_size].astype(np.float32),
                                               [batch_size, 1, 1280, 1]), test_labels[
                                               (step - 1) * batch_size:step * batch_size].astype(np.float32)
-                acc = sess.run([self.accuracy], feed_dict={self.X: batch_x, self.Y: batch_y})
+                acc = sess.run([self.accuracy, self.prediction], feed_dict={self.X: batch_x, self.Y: batch_y})
                 total_accuracy = total_accuracy + acc[0]
+                result.apppend(np.array(acc[1]))
         total_accuracy *= batch_size
-        print('Total accuracy is {:.2f}%'.format(total_accuracy/100))
+        print('Total accuracy is {:.4f}'.format(total_accuracy/40))
         sess.close()
 
+        result = np.array(result)
+        np.save('result_ver1.npy', result)
+
 def main():
-    
+    '''
     MNIST = SpherePHD()
     MNIST.simple_cnn(SUBDIVISION)
     MNIST.train()
     MNIST.test()
+    '''
+    Stanford2D3D = SpherePHD()
+    Stanford2D3D.autoencoder(SUBDIVISION)
+    Stanford2D3D.train()
 
 if __name__ == '__main__':
     main()
